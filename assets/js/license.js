c75d7ec88626f2a0a4bd74ced77dcb4e63ada471
linxu <1062934106@qq.com>
2023-02-24 14:35:47 +0800

    1

--- assets/js/license.js
+++ assets/js/license.js
@@ -1,5 +1,5 @@
 /*
-  PaperMod v7
+  LinsTheme
   License: MIT https://https://github.com/DawnEver/hugo-LinsTheme/blob/master/LICENSE
   Copyright (c) 2020 nanxiaobei and adityatelange
   Copyright (c) 2021-2023 adityatelange
